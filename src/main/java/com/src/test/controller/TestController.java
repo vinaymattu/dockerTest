7351a374eea4c6809c32db406e5a2769c677a184
vinaymattu <vmattu9@gmail.com>
2023-02-06 02:58:58 -0500

    first commit

---
+++ src/main/java/com/src/test/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.src.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@RequestMapping("/test")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+}
